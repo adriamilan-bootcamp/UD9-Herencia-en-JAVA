ad8678bd4b6f5fad6906a1c6f79a1436bb6d6164
Clara Ogalla <claraogalla@gmail.com>
2022-07-14 10:21:24 +0200

    v0.1 Clase Serie

---
+++ UD09_01/src/TA09_02/Serie.java
@@ -0,0 +1,75 @@
+package TA09_02;
+
+public class Serie {
+	protected String titulo = "";
+	protected int nTemporadas = 3;
+	protected boolean entregado = false;
+	protected String creador = "";
+	
+	public Serie() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Serie(String titulo, String creador) {
+		super();
+		this.titulo = titulo;
+		this.creador = creador;
+	}
+
+	public Serie(String titulo, int nTemporadas, String creador) {
+		super();
+		this.titulo = titulo;
+		this.nTemporadas = nTemporadas;
+		this.creador = creador;
+	}
+
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	/**
+	 * @return the nTemporadas
+	 */
+	public int getnTemporadas() {
+		return nTemporadas;
+	}
+
+	/**
+	 * @param nTemporadas the nTemporadas to set
+	 */
+	public void setnTemporadas(int nTemporadas) {
+		this.nTemporadas = nTemporadas;
+	}
+
+	/**
+	 * @return the creador
+	 */
+	public String getCreador() {
+		return creador;
+	}
+
+	/**
+	 * @param creador the creador to set
+	 */
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+
+	@Override
+	public String toString() {
+		return "Serie [titulo=" + titulo + ", Numero de temporadas=" + nTemporadas + ", entregado=" + entregado + ", creador="
+				+ creador + "]";
+	}
+	
+
+}
