4c679e62809cc96c7379d78d77a3af3c10f3a05b
Clara Ogalla <claraogalla@gmail.com>
2022-07-14 12:52:04 +0200

    V0.5

--- UD09_01/src/TA09_02/mainApp.java
+++ UD09_01/src/TA09_02/mainApp.java
@@ -4,7 +4,13 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Serie lSeries[] = new Serie[5];
+		
+		lSeries[0] = new Serie("titulo", 3, "Creador");
+		lSeries[1] = new Serie("titulo2", 12, "Ram");
+		lSeries[2] = new Serie("titulo3", 7, "Diosito");
+		lSeries[3] = new Serie("titulo4", 6, "Kirito");
+		lSeries[4] = new Serie("titulo5", 24, "Rem");
 	}
 
 }
