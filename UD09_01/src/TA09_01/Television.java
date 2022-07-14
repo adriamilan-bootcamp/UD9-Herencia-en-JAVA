1d7629b70b303db8c02c3117acc211ed93b2a26a
Clara Ogalla <claraogalla@gmail.com>
2022-07-14 09:48:39 +0200

    V0.2 A falta de concordancia con clase padre

--- UD09_01/src/TA09_01/Television.java
+++ UD09_01/src/TA09_01/Television.java
@@ -4,19 +4,27 @@ public class Television extends Electrodomestico {
 	private int pulgadas = 20;
 	private boolean TDT = false;
 	
-	
 	public Television() {
 		// TODO Auto-generated constructor stub
 	}
 
 
+	public Television(double precio, double peso) {
+		super(precio, peso);
+	}
+	
+	public Television(int pulgadas, boolean TDT, double precio ,String color, char consumo, double peso) {
+		super(precio, color, peso, consumo);
+		this.pulgadas = pulgadas;
+		this.TDT = TDT;
+	}
+	
 	/**
 	 * @return the pulgadas
 	 */
 	public int getPulgadas() {
 		return pulgadas;
 	}
-
 
 	/**
 	 * @return the tDT
@@ -25,4 +33,17 @@ public class Television extends Electrodomestico {
 		return TDT;
 	}
 
+	
+	public double precioFinal() {
+		if(this.pulgadas > 40) {
+			//Incrementamos el precio un 30%
+			precio = (precio * 1.30);
+		}
+		if(this.TDT == true ) {
+			precio += 50;
+		}
+		return this.precio;
+	}
+	
+	
 }
