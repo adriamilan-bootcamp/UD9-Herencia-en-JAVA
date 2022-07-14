d2a25c6ac25ab1f6623ac0b39fbddcb8cc71d92a
Clara Ogalla <claraogalla@gmail.com>
2022-07-14 11:59:20 +0200

    V0.1

---
+++ UD09_01/src/TA09_03/Libro.java
@@ -0,0 +1,75 @@
+package TA09_03;
+
+public class Libro {
+	private String ISBN;
+	private String Titulo;
+	private String Autor;
+	private int nPaginas;
+	
+	public Libro(String iSBN, String titulo, String autor, int nPaginas) {
+		super();
+		ISBN = iSBN;
+		Titulo = titulo;
+		Autor = autor;
+		this.nPaginas = nPaginas;
+	}
+
+	/**
+	 * @return the iSBN
+	 */
+	public String getISBN() {
+		return ISBN;
+	}
+
+	/**
+	 * @param iSBN the iSBN to set
+	 */
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return Titulo;
+	}
+
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+
+	/**
+	 * @return the autor
+	 */
+	public String getAutor() {
+		return Autor;
+	}
+
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(String autor) {
+		Autor = autor;
+	}
+
+	/**
+	 * @return the nPaginas
+	 */
+	public int getnPaginas() {
+		return nPaginas;
+	}
+
+	/**
+	 * @param nPaginas the nPaginas to set
+	 */
+	public void setnPaginas(int nPaginas) {
+		this.nPaginas = nPaginas;
+	}
+	
+	
+	
+}
