5e927cef21cf57756057b411fe8c00c81f2500d9
Clara Ogalla <claraogalla@gmail.com>
2022-07-14 09:38:43 +0200

    v0.1

--- UD09_01/src/TA09_01/Television.java
+++ UD09_01/src/TA09_01/Television.java
@@ -1,9 +1,28 @@
 package TA09_01;
 
-public class Television {
-
+public class Television extends Electrodomestico {
+	private int pulgadas = 20;
+	private boolean TDT = false;
+	
+	
 	public Television() {
 		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * @return the pulgadas
+	 */
+	public int getPulgadas() {
+		return pulgadas;
+	}
+
+
+	/**
+	 * @return the tDT
+	 */
+	public boolean isTDT() {
+		return TDT;
 	}
 
 }
