e916e3f415f71b9c02288b3670ef64598d1daeb3
Celestinvib <celestinvib@gmail.com>
2022-07-15 10:38:20 +0200

    Clase Profesor v1.0

--- UD09_01/src/TA09_05/Profesor.java
+++ UD09_01/src/TA09_05/Profesor.java
@@ -2,35 +2,25 @@ package TA09_05;
 
 import TA09_02.Entregable;
 
-public class Profesor extends Persona implements Faltas {
-	
-	private String materiasDisponibles[] = {"matematicas","filosofia","fisica"};
-		
+public class Profesor extends Persona implements Faltas {		
 	/*Atributos de la clase */	
-	private String materia = "";
+	private Materia materia;
 	
 	private boolean disponible =  true;
 	
-	public Profesor () {
-		
+	public Profesor (String nombre, int edad, char sexo) {
+		super(nombre, edad, sexo);		
 	}
 	
-	public Profesor(String nombre, int edad, char sexo, String materia , boolean disponible) {
+	public Profesor(String nombre, int edad, char sexo, Materia materia) {
 		super(nombre, edad, sexo);
-		this.disponible = disponible;
-
-		boolean mDisponible = checkMateria(materia);	
-		
-		if(mDisponible) {
-			this.materia = materia;
-		}
-		
+		this.materia = materia;
 	}
 	
 	/**
 	 * @return the materia
 	 */
-	public String getMateria() {
+	public Materia getMateria() {
 		return materia;
 	}
 
@@ -38,27 +28,11 @@ public class Profesor extends Persona implements Faltas {
 	/**
 	 * @param materia the materia to set
 	 */
-	public void setMateria(String materia) {
+	public void setMateria(Materia materia) {
 		this.materia = materia;
 	}
 
-	/**
-     * Método que comprueba si la materia introducida por el usuario esta disponible o no 
-     * @param materia materia introducida
-     * @return un boolean indicando si la materia esta disponible
-    */
-	public boolean checkMateria(String materia) {
-		boolean mDisponible = false;
-		
-		for (int i = 0; i < materiasDisponibles.length; i++) {
-			if(materiasDisponibles[i].equals(materia)) {
-				mDisponible = true;
-			}
-		}
-		
-		return mDisponible;
-	}
-	
+
 	@Override
 	public boolean Faltas () {
 		boolean disponible = true;
