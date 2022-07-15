aac2c534c10226ffd9b2e82261add7cbb37765eb
Clara Ogalla <claraogalla@gmail.com>
2022-07-15 10:09:50 +0200

    TA09_05

---
+++ UD09_01/src/TA09_05/Aula.java
@@ -0,0 +1,12 @@
+package TA09_05;
+
+public class Aula {
+	private boolean disponible = false;
+	private String materia = "";
+	private int nAlumnosMax = 25;
+	
+	public Aula() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
