f4e44e6b834bfc4af2276e8439af9cdb67b53137
clarax98 <100277391+clarax98@users.noreply.github.com>
2022-07-15 10:49:49 +0200

    Update Aula.java

--- UD09_01/src/TA09_05/Aula.java
+++ UD09_01/src/TA09_05/Aula.java
@@ -9,4 +9,46 @@ public class Aula {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	//SE puede hacer clase si el numero de alumnos en el aula es superior o igual a la mitad de la capacidad de la clase
+	public boolean comprobarClase(int nAlumnos) {
+		if((int)(this.nAlumnosMax/2) >= nAlumnos) {
+			return true;
+		}
+		return false;
+	}
+
+
+	/**
+	 * @return the materia
+	 */
+	public String getMateria() {
+		return materia;
+	}
+
+
+	/**
+	 * @param materia the materia to set
+	 */
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+
+	/**
+	 * @return the nAlumnosMax
+	 */
+	public int getnAlumnosMax() {
+		return nAlumnosMax;
+	}
+
+
+	/**
+	 * @param nAlumnosMax the nAlumnosMax to set
+	 */
+	public void setnAlumnosMax(int nAlumnosMax) {
+		this.nAlumnosMax = nAlumnosMax;
+	}
+	
+	
 }
