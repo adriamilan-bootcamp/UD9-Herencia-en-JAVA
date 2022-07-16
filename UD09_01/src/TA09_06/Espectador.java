5d51a6d2e2c7d10726bd8750a24d5e2b613c8427
Adrià Milan <adria4milan@gmail.com>
2022-07-16 22:47:21 +0200

    Espectador v1.0

--- UD09_01/src/TA09_06/Espectador.java
+++ UD09_01/src/TA09_06/Espectador.java
@@ -2,4 +2,66 @@ package TA09_06;
 
 public class Espectador {
 
+	/* Atributos de la clase */
+	private String nombre;
+	private int edad;
+	private double dinero;
+	
+	public Espectador() {
+		
+	}
+	
+	public Espectador(String nombre, int edad, double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
+
+	// Getters & Setters
+	
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * @return the dinero
+	 */
+	public double getDinero() {
+		return dinero;
+	}
+
+	// Método para saber si el espectador tiene suficiente dinero para ver la película
+	public boolean dineroSuficiente(double costoEntrada) {
+		
+		// Si el dinero que tiene el espectador es mayor o igual al costo de la entrada.
+		if(this.dinero >= costoEntrada) {
+			return true; // Devuelve true
+		} else {
+			return false; // Devuelve false
+		}
+		
+	}
+	
+	// Método para saber si el espectador tiene la edad suficiente para ver la película
+	public boolean edadSuficiente(int edadPelicula) {
+		
+		// Si la edad es igual o mayor a la mínima para ver la película
+		if(this.edad >= edadPelicula) {
+			return true; // Devuelve true
+		} else {
+			return false; // Devuelve false
+		}
+		
+	}
+ 	
 }
