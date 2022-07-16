6c224f981f5dfd34c8921fd69eb7c43e83839e12
Adrià Milan <adria4milan@gmail.com>
2022-07-16 22:31:50 +0200

    mainApp v2.0

--- UD09_01/src/TA09_04/mainApp.java
+++ UD09_01/src/TA09_04/mainApp.java
@@ -1,12 +1,34 @@
 package TA09_04;
 
+import javax.swing.JOptionPane;
+
 public class mainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Raices n1 = new Raices(2,5,6);
-		n1.calcular();
+
+		inputOutput();
 		
 	}
 
+	public static void inputOutput() {
+		
+		// Pedimos al usuario la cantidad de calculos que quiere realizar
+		int numeroCalculos = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número de caluclos que quieres realizar."));
+		
+		// Creamos cada objeto y mostramos los calculos realizados.
+		for (int i = 0; i < numeroCalculos; i++) {
+			/*
+			 *  Creamos el objeto
+			 *  - Coeficiente a
+			 *  - Coeficiente b
+			 *  - Coeficiente c
+			 */
+			Raices raiz = new Raices(Double.parseDouble(JOptionPane.showInputDialog("Introduce el primer coeficiente.")), Double.parseDouble(JOptionPane.showInputDialog("Introduce el segundo coeficiente.")), Double.parseDouble(JOptionPane.showInputDialog("Introduce el tercer coeficiente.")));
+			
+			// Mostramos los calculos
+			raiz.calcular();
+		}
+		
+	}
+	
 }
