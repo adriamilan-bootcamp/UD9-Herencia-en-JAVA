a26260ab16bdaf07a15226b3e7d125ce12bcfad6
Celestinvib <celestinvib@gmail.com>
2022-07-17 17:01:21 +0200

    ex04 Clase Raices v2.0

--- UD09_01/src/TA09_04/Raices.java
+++ UD09_01/src/TA09_04/Raices.java
@@ -1,29 +1,45 @@
 package TA09_04;
 
 public class Raices {
+	
+	/*Atributos*/
 	private double a, b, c;
 	
-	public Raices() {
-		// TODO Auto-generated constructor stub
-	}
-
+	/*Constructor */
+	/**
+	 * @param a
+	 * @param b
+	 * @param c
+	 */
 	public Raices(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
-	}
+	}	
 
+	/**
+     * Metodo que calcula el valor discriminante
+     * @return el valor discriminante
+    */	
 	public double getDiscriminante() {
-		return (Math.pow(this.b, 2))-4*this.a*this.c;
+		return ((Math.pow(this.b, 2))-4)*this.a*this.c;
 	}
 	
+	/**
+     * Metodo que llama a getDiscriminante() para averiguar si la raiz tiene 2 soluciones
+     * @return booleano que indica si tiene 2 soluciones
+    */	
 	public boolean tieneRaices() {
 		if(getDiscriminante() >= 0) {
 			return true;
 		}
 		return false;
 	}
-	
+
+	/**
+     * Metodo que llama a getDiscriminante() para averiguar si tiene
+     * @return booleano que indica si tiene 2 soluciones
+    */	
 	public boolean tieneRaiz() {
 		if(getDiscriminante() == 0) {
 			return true;
@@ -31,29 +47,33 @@ public class Raices {
 		return false;
 	}
 	
+	/**
+     * Metodo que averigua y muestra si la raiz tiene 1 , 2 o ninguna solucion
+    */	
 	public void calcular () {
-		if (getDiscriminante() >= 0) {
-	        // Una solucion
-	        if(getDiscriminante() == 0){
-	        	System.out.println("Solucion Unica:" + ((-b) - (4 * a * c)) / (2 * a));
-	        // Dos soluciones
-	        }else{
-	        	System.out.println("Solucion 1:" + ((-b) + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
-	        	System.out.println("Solucion 2:" + ((-b) - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
-	        }
-	    } else {
-	        // Sin solucion
+		
+		if (tieneRaices()) {  // Dos soluciones        
+        	obtenerRaices();
+		}else if(tieneRaiz()){// Una solucion
+	        	obtenerRaiz();     
+	    } else { // Sin solucion       
         	System.out.println("Sin solucion");
 	    }	
 	}
-	
+
+	/**
+     * Metodo que muestra las 2 soluciones de la raiz
+    */	
 	public void obtenerRaices () {
-		System.out.println("Solucion 1:" + ((-b) + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
-	    System.out.println("Solucion 2:" + ((-b) - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
+		System.out.println("Solucion 1: " + ((this.b) + Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a));
+	    System.out.println("Solucion 2: " + ((-this.b) - Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a));
 	}
-	
-	public void obtenerRaiaz () {
-		System.out.println("Solucion Unica:" + ((-b) - (4 * a * c)) / (2 * a));
+
+	/**
+     * Metodo que muestra la solucion de la raiz
+    */	
+	public void obtenerRaiz () {
+		System.out.println("Solucion Unica: " + ((-this.b) - (4 * this.a * this.c)) / (2 * this.a));
 	}
 	
 }
