e289b3c40a1c338ba0e442dadba167d22945a4ab
Celestinvib <celestinvib@gmail.com>
2022-07-20 20:05:57 +0200

    mainApp v1.4

--- UD09_01/src/TA09_06/mainApp.java
+++ UD09_01/src/TA09_06/mainApp.java
@@ -5,10 +5,10 @@ public class mainApp {
 	public static void main(String[] args) {
 		Pelicula c1 = new Pelicula("El regreso del jedi", 120, 12, "Paco");
 		Cine cine = new Cine(6.90, c1);
-		//Ahora el cine esta vacio
-		cine.printSala();
-		cine.venderEntradas();
-		cine.printSala();
+		
+		cine.printSala();//se muestra el cine esta vacio
+		cine.venderEntradas();//Se venden las entradas a los espectadores
+		cine.printSala(); //Se muestra la sala con los espectadores que han conseguido entrar
 	}
 
 }
